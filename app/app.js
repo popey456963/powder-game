cf479e6c8821e891d3bba0b1ea1458059ea00b6a
aappleton8 <aappleton8@users.noreply.github.com>
2019-01-06 21:54:24 +0000

    Update Styling 2
    - Enable the canvas size to be set.
    - Add start and stop generation buttons.
    - Alter the margin around buttons.
    - Add ids to the main page elements.
    - Update the 'Globals' object.

--- app/app.js
+++ app/app.js
@@ -1,16 +1,20 @@
 // Allows us to use Promises, Symbols, etc.
 import "babel-polyfill"
+import "query-string"
 import "../styles/css/style.scss"
 
 const Game = require('./modules/Game')
 
-const game = new Game(Globals.canvas)
+const game = new Game(Globals.ids.canvas)
 game.start()
 
-document.getElementById('start').onclick = function() { game.start() }
-document.getElementById('stop').onclick = function() { game.stop() }
-document.getElementById('tick').onclick = function() { game.tick() }
-document.getElementById('reset').onclick = function() { game.reset() }
+document.getElementById(Globals.ids.start).onclick = function() { game.start() }
+document.getElementById(Globals.ids.stop).onclick = function() { game.stop() }
+document.getElementById(Globals.ids.tick).onclick = function() { game.tick() }
+document.getElementById(Globals.ids.reset).onclick = function() { game.reset() }
 
-document.getElementById('makeFloor').onclick = function() { game.makeFloor() }
-document.getElementById('removeFloor').onclick = function() { game.removeFloor() } 
+document.getElementById(Globals.ids.makeFloor).onclick = function() { game.makeFloor() }
+document.getElementById(Globals.ids.removeFloor).onclick = function() { game.removeFloor() } 
+
+document.getElementById(Globals.ids.startGenerate).onclick = function() { game.startGenerate() }
+document.getElementById(Globals.ids.stopGenerate).onclick = function() { game.stopGenerate() } 
